101e7a074c52ed19c5408667148ea5be10cda153
Danice123 <mitchelldanice@gmail.com>
2014-10-20 19:13:51 -0400

    Abstract Classes

---
+++ Monster.h
@@ -0,0 +1,41 @@
+#ifndef __Monster_
+#define __Monster_
+#include "Skill.h"
+#include <vector>
+
+enum type{WATER, GRASS, FIRE};
+
+class Monster {
+public:
+	Monster();
+
+	virtual void damageMonster(int, type) = 0;
+
+	std::vector<Skill> getSkills() { return skills; }
+	std::vector<Skill> getStatusEffects() { return statusEffects; }
+
+	int getAttack() { return attack; }
+	int getDefense() { return defense; }
+	int getHealth() { return health; }
+	int getSpeed() { return speed; }
+	int getCrit() { return crit; }
+	int getMana() { return mana; }
+
+	int getCurrentHealth() { return currentHealth; }
+	int getCurrentMana() { return currentMana; }
+
+	void setAttack(int i) { attack = i; }
+	void setDefense(int i) { defense = i; }
+	void setHealth(int i) { health = i; }
+	void setSpeed(int i) { speed = i; }
+	void setCrit(int i) { crit = i; }
+	void setMana(int i) { mana = i; }
+
+	void modifyCurrentMana(int i) { currentMana = i; }
+private:
+	type creature_type;
+	int attack, defense, health, speed, crit, mana, currentHealth, currentMana;
+	std::vector<Skill> skills;
+	std::vector<Skill> statusEffects;
+};
+#endif
---
+++ Skill.h
@@ -0,0 +1,18 @@
+#ifndef __Skill_
+#define __Skill_
+#include "Monster.h"
+#include <string>
+
+class Skill {
+public:
+	Skill();
+	
+	virtual std::string describe() = 0;
+	virtual void apply(Monster) = 0;
+	virtual void tick(Monster) = 0;
+	virtual int getManaCost() { return manaCost; }
+private:
+	int manaCost;
+};
+
+#endif
